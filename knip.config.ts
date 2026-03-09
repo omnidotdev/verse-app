a9c4483214be46b652c775fe6042bbf05ed9e300
Brian Cooper <brian@brian-cooper.com>
2026-03-09 02:17:50 -0400

    build(deps): upgrade deps

--- knip.config.ts
+++ knip.config.ts
@@ -5,10 +5,9 @@ import type { KnipConfig } from "knip";
  * @see https://knip.dev/overview/configuration
  */
 const knipConfig: KnipConfig = {
-	entry: ["src/routes/**/*.{ts,tsx}", "src/main.tsx"],
+	entry: ["src/routes/**/*.{ts,tsx}"],
 	project: ["src/**/*.{ts,tsx,css}"],
 	ignoreExportsUsedInFile: true,
-	ignore: ["src/routeTree.gen.ts"],
 	tags: ["-knipignore"],
 };
 
